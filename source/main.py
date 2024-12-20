902188edcfec26213f21ffa4a2008af98e88d588
ClartTheRoyalAcademyOfArt <lmk.kadlecek@gmail.com>
2024-12-19 20:34:53 -0700

    initial commit

---
+++ source/main.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Game:
+    def __init__(self):
+        
+        self.clock = pygame.time.Clock()
+
+    def main_game_loop(self):
+        # Main game loop
+        self.mainloop = True
+        while self.mainloop == True:
+            pass
+
+if __name__ == "__main__":
+    game = Game()
+    game.main_game_loop()
