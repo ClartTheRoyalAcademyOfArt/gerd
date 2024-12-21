1de489cac7187a87c71f65f548caa4b3a38e608e
ClartTheRoyalAcademyOfArt <lmk.kadlecek@gmail.com>
2024-12-21 16:31:44 -0700

    indent change

--- source/main.py
+++ source/main.py
@@ -1,16 +1,32 @@
 import pygame
 
+
+
+
+
 class Game:
+
     def __init__(self):
         
+        # Game Consts
         self.clock = pygame.time.Clock()
 
+
+
     def main_game_loop(self):
+
         # Main game loop
         self.mainloop = True
+
         while self.mainloop == True:
+
             pass
 
+
+
+
+
 if __name__ == "__main__":
+
     game = Game()
     game.main_game_loop()
